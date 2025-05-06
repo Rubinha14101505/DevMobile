c8b672d1099504ff9a3a9e80021be6b9131bdc93
MykyBalb <mykaelbalbino06@gmail.com>
2025-05-05 21:54:17 -0300

    Aula 05/05

---
+++ Projetos/Ex02Localizacao/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, StatusBar } from 'react-native';
+import MapView from "react-native-maps";
+import styles from "./styles";
+
+StatusBar.setBarStyle("dark-content");
+
+export default () => (
+  <View style={styles.container}>
+
+    <MapView 
+      style={styles.mapView}
+      showsUserLocation={true}
+      followsUserLocation={true}
+    />
+  </View>
+);
